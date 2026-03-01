c02541cfe23395ac45f18476e18231ef0a345f61
sunm2n <sunmin8410@naver.com>
2026-03-01 18:19:24 +0900

    초기 세팅

---
+++ src/main/java/com/example/websoketvtualtreadtest/WebsoketVtualTreadTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.websoketvtualtreadtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsoketVtualTreadTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsoketVtualTreadTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/websoketvtualtreadtest/WebsoketVtualTreadTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websoketvtualtreadtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsoketVtualTreadTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
