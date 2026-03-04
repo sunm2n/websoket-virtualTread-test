95f16546e52f3c952f82e393b764049f4ed4970b
sunm2n <sunmin8410@naver.com>
2026-03-04 11:34:13 +0900

    refactor: 구버전 파일 삭제

--- k6/check-threads.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl -s localhost:8080/actuator/metrics/jvm.threads.live \
-  | python3 -c "
-import sys, json
-d = json.load(sys.stdin)
-v = next(m['value'] for m in d['measurements'] if m['statistic'] == 'VALUE')
-print(f'JVM live threads: {int(v)}')
-"
